0ba5d21c952b2855dc1eecc14614e8597d5cb8d5
Lester A John <LesterAJohn@aol.com>
2022-08-28 10:21:54 -0500

    Added Install Scripts for Mongo, rabbitMQ and pip

---
+++ src/OSServices/mongodbInstallation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# mongodb installation
+
+sudo apt install gnupg
+
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install mongodb-org -y
+ 
